9ff19a7806e2eb0bac3287156db9429bafcf6048
hari-cat <heeman8374@gamil.com>
2026-09-20 21:19:22 +0900

    feat: 헤더 백버튼 아이콘 및 뒤로가기 기능 추가

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,8 +1,20 @@
+import { useNavigate } from "react-router";
+import leftArrow from "../assets/icon/left-arrow-icon.png";
+
 function Header() {
+  const navigate = useNavigate();
+  const handlebackbutton = () => {
+    navigate(-1);
+  };
   return (
     <div className="text-center p-3 shadow-sm">
-      <div className="text-xl">
-        아파트민원<span className="text-amber-500 text-xl"> 각</span>
+      <div className="text-xl relative">
+        <div className="absolute left-1 top-2" onClick={handlebackbutton}>
+          <img src={leftArrow} width={"14px"} />
+        </div>
+        <div>
+          아파트민원<span className="text-amber-500 text-xl"> 각</span>
+        </div>
       </div>
     </div>
   );
